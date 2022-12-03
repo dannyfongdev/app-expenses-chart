e8b43c2feb60c997cac1edf0fe0e69eca1cebf27
Danny Fong <danny.fong.dev@outlook.com>
2022-12-03 15:12:54 -0800

    Added alt attribute

--- src/components/MyBalance.js
+++ src/components/MyBalance.js
@@ -4,7 +4,7 @@ function MyBalance() {
     <div className={styles.container}> 
     <div>
       <p className={styles.label}>My balance</p>
-      <p className={styles.amount}>$921.48</p></div><div><img src="../images/logo.svg" /></div>
+      <p className={styles.amount}>$921.48</p></div><div><img src="../images/logo.svg" alt="logo" /></div>
     </div>
   );
 }
